07b446adcf22d8b96f18acc032a0cd609975cf6b
mook <mookjpy@gmail.com>
2018-02-06 21:55:02 +0900

    complete ex10

--- ch01/ex10/fetchall.go
+++ ch01/ex10/fetchall.go
@@ -7,18 +7,34 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"path"
 )
 
 func main() {
+	dirPath, err := getOutputDirPath()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "parameter error: %s", err)
+		os.Exit(1)
+	}
+
+	outputFile, err := createOutputFile(dirPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not create file, error: %s", err)
+		os.Exit(1)
+	}
+
 	start := time.Now()
 	ch := make(chan string)
-	for _, url := range os.Args[1:] {
+	for _, url := range os.Args[2:] {
 		go fetch(url, ch) // start goroutine
 	}
-	for range os.Args[1:] {
-		fmt.Println(<-ch) // receive from ch channel
+	for range os.Args[2:] {
+		res := <- ch
+		fmt.Println(res)
+		outputFile.WriteString(fmt.Sprintf("%s\n", res))
 	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	fmt.Printf("The result was written to `%s`", outputFile.Name())
 }
 
 func fetch(url string, ch chan <- string) {
@@ -37,4 +53,27 @@ func fetch(url string, ch chan <- string) {
 	}
 	secs := time.Since(start).Seconds()
 	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
+
+
+// TODO: how to write returning error
+func getOutputDirPath() (string, error) {
+	outputDirPath := os.Args[1]
+	_, err := os.Stat(outputDirPath)
+	if err != nil {
+		return "", err // TODO: これでいいのか？
+	} else {
+		return outputDirPath, nil
+	}
+}
+
+func createOutputFile(dirPath string) (*os.File, error) {
+	fileName := fmt.Sprintf("output_%s.txt", time.Now().Format("2006-01-02T15:04:05"))
+	filePath := path.Join(dirPath, fileName)
+	outputFile, err := os.Create(filePath)
+	if err != nil {
+		return nil, err
+	} else {
+		return outputFile, nil
+	}
 }
--- ch01/ex10/run.bash
+++ ch01/ex10/run.bash
@@ -1,3 +1,15 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
-go run ${SCRIPT_DIR}/fetchall.go https://golang.org http://gopl.io https://godoc.org
+
+# Directory to save output
+OUTPUT_DIR="${SCRIPT_DIR}/tmp"
+mkdir -p ${OUTPUT_DIR}
+
+# Url
+URL_01=https://cookpad.com/search/%E3%83%90%E3%83%AC%E3%83%B3%E3%82%BF%E3%82%A4%E3%83%B3
+URL_02=https://cookpad.com/search/%E3%82%AC%E3%83%88%E3%83%BC%E3%82%B7%E3%83%A7%E3%82%B3%E3%83%A9
+
+echo "=== First fetchall"
+go run ${SCRIPT_DIR}/fetchall.go ${OUTPUT_DIR} ${URL_01} ${URL_02}
+echo "=== Second fetchall"
+go run ${SCRIPT_DIR}/fetchall.go ${OUTPUT_DIR} ${URL_01} ${URL_02}
