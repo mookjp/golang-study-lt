43d2c253c631a35343e667d5a46a7d7322afff5c
mook <mookjpy@gmail.com>
2018-02-06 20:08:49 +0900

    complete ex07

---
+++ ch01/ex07/fetch.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"net/http"
+	"fmt"
+	"io"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		// https://golang.org/doc/effective_go.html#if
+		// statement initialization ができる
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
---
+++ ch01/ex07/run.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+echo "=== http://gopl.io"
+go run ${SCRIPT_DIR}/fetch.go http://gopl.io
+echo "=== http://bad.gopl.io"
+go run ${SCRIPT_DIR}/fetch.go http://bad.gopl.io
