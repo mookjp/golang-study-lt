271b3de27eab242db4cb608259fb91eb66046cc6
mook <mookjpy@gmail.com>
2018-02-15 10:42:09 +0900

    chore: add case

--- ch01/ex12/lissajous-server_test.go
+++ ch01/ex12/lissajous-server_test.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"testing"
 	"bytes"
+	"net/http"
+	"testing"
 )
+
+// TODO: mainをgoroutineで2回呼べない。。
+//func TestServerReturnsResponseWithoutParams(t *testing.T) {
+//	go func() {
+//		// run server
+//		main()
+//	}()
+//
+//	_, err := http.Get("http://localhost:8000")
+//	if err != nil {
+//		t.Errorf("Got error in requesting. %v", err)
+//	}
+//}
+
+func TestServerReturnsResponseWithParams(t *testing.T) {
+	go func() {
+		// run server
+		main()
+	}()
+
+	_, err := http.Get("http://localhost:8000/?size=400&cycles=120")
+	if err != nil {
+		t.Errorf("Got error in requesting. %v", err)
+	}
+}
 
 func TestLissajous(t *testing.T) {
 	params := lissajousParams{
