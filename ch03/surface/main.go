bfa634a7580fd25f8bc77f482f43a30c7450db13
mook <mookjpy@gmail.com>
2018-03-15 23:43:10 +0900

    fix code

--- ch03/surface/main.go
+++ ch03/surface/main.go
@@ -17,9 +17,9 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30度), cos(30度)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://w3.org/2000/svg' "+
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
+		"width='%d' height='%d'>\n", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
