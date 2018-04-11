490a15c0849f8def4eb76b982b1c5b44bc063f92
mook <mookjpy@gmail.com>
2018-04-11 18:44:37 +0900

    add ex05

--- ch03/ex05/main.go
+++ ch03/ex05/main.go
@@ -36,8 +36,8 @@ func mandelbrot(z complex128) color.Color {
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			return color.RGBA{R: 140, G: 190, B: 190, A: contrast*n}
 		}
 	}
-	return color.Black
+	return color.RGBA{R: 140, G: 190, B: 190, A: 100}
 }
