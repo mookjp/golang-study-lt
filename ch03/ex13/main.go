0885c3cea9e002ada76178d51697e15bd444059c
mook <mookjpy@gmail.com>
2018-04-13 02:35:13 +0900

    tmp

---
+++ ch03/ex13/main.go
@@ -0,0 +1,45 @@
+package ex13
+
+//const (
+//	_ = 1000 * iota
+//	KB
+//	MB
+//	GB
+//	TB
+//	PB
+//)
+
+const (
+	KB = 1000
+	MB = 1000 * KB
+	GB = 1000 * MB
+	TB = 1000 * GB
+	PB = 1000 * TB
+	EB = 1000 * PB
+	ZB = 1000 * EB
+	YB = 1000 * ZB
+)
+
+type c struct {
+	KB uint64
+	MB uint64
+	GB uint64
+	TB uint64
+	PB uint64
+	EB uint64
+	ZB uint64
+	YB uint64
+}
+
+func consts() c {
+	return c{
+		KB: KB,
+		MB: MB,
+		GB: GB,
+		TB: TB,
+		PB: PB,
+		EB: EB,
+		ZB: ZB,
+		YB: YB,
+	}
+}
---
+++ ch03/ex13/main_test.go
@@ -0,0 +1,25 @@
+package ex13
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestConsts(t *testing.T) {
+	expected := c{
+		KB: 1000,
+		MB: 1000000,
+		GB: 1000000000,
+		TB: 1000000000000,
+		PB: 1000000000000000,
+		EB: 1000000000000000000,
+		ZB: 1000000000000000000000,
+		YB: 1000000000000000000000000,
+	}
+	res := consts()
+	fmt.Printf("expected: %v\n", expected)
+	fmt.Printf("res: %v\n", res == expected)
+	if res != expected {
+		t.Errorf("actual: %v\n", res)
+	}
+}
