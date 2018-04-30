6ad173f4ec6d53d9ef09705c41595cf6e1bae13a
mook <mookjpy@gmail.com>
2018-04-30 22:01:26 +0900

    tmp; ok for some cases

--- ch04/ex01/main.go
+++ ch04/ex01/main.go
@@ -1,14 +1,19 @@
 package ex01
 
-func main() {
-
-}
-
 func countBitDiff(srcBytes *[32]byte, targetBytes *[32]byte) int {
 	var diffCount int
+
 	for i, v := range targetBytes {
-		if v != srcBytes[i] {
-			diffCount++
+		for bitN := 0; bitN < 8; bitN++ {
+			var bitDiff bool
+			if bitN == 0 {
+				bitDiff = int(v)&1 == int(srcBytes[i])&1
+			} else {
+				bitDiff = int(v)&2*bitN == int(srcBytes[i])&2*bitN
+			}
+			if !bitDiff {
+				diffCount++
+			}
 		}
 	}
 	return diffCount
--- ch04/ex01/main_test.go
+++ ch04/ex01/main_test.go
@@ -1,10 +1,15 @@
 package ex01
 
 import (
+	"encoding/binary"
 	"fmt"
 	"os"
 	"testing"
 )
+
+func TestPrint(t *testing.T) {
+	fmt.Fprintf(os.Stdout, "x: %v\n", binary.BigEndian.Uint64([]byte{'x'}))
+}
 
 func TestBitCount(t *testing.T) {
 	var tests = []struct {
@@ -12,9 +17,13 @@ func TestBitCount(t *testing.T) {
 		input2   [32]byte
 		expected int
 	}{
-		{[32]byte{}, [32]byte{}, 0},
-		{makeSequence(1), makeSequence(1), 0},
-		{makeSequence(1), makeSequence(2), 1},
+		//{[32]byte{}, [32]byte{}, 0},
+		//{makeSequence(1), makeSequence(1), 0},
+		//{[32]byte{1}, [32]byte{}, 1},
+		{[32]byte{3}, [32]byte{}, 2},
+		//{[32]byte{4}, [32]byte{}, 3},
+		//{[32]byte{255}, [32]byte{}, 8},
+		//{[32]byte{255, 1}, [32]byte{}, 9},
 	}
 	for _, test := range tests {
 		fmt.Fprintf(os.Stdout, "input: %v %v expected: %v\n", test.input1, test.input2, test.expected)
