b7bfba95b8bfa7a278e51c559bd8ed016631a472
mook <mookjpy@gmail.com>
2018-07-22 21:41:34 +0900

    Change to use close write

--- ch08/ex03/main.go
+++ ch08/ex03/main.go
@@ -33,7 +33,7 @@ func main() {
 		done <- struct{}{} // signal the main goroutine
 	}()
 	mustCopy(conn, os.Stdin)
-	conn.Close()
+	conn.CloseWrite()
 	<-done // wait for background goroutine to finish
 }
 
