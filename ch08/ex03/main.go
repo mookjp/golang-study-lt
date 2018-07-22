90130add1f87451ae2668c02cf6ead57f29c6b8e
mook <mookjpy@gmail.com>
2018-07-22 20:35:01 +0900

    change to use dial tcp method

---
+++ ch08/ex03/main.go
@@ -0,0 +1,43 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 227.
+
+// Netcat is a simple read/write client for TCP servers.
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+//!+
+func main() {
+	serverAddr, addrErr := net.ResolveTCPAddr("tcp", "localhost:8000")
+	if addrErr != nil {
+		log.Fatal(addrErr)
+	}
+	conn, connErr := net.DialTCP("tcp", nil, serverAddr)
+	if connErr != nil {
+		log.Fatal(connErr)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done // wait for background goroutine to finish
+}
+
+//!-
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
