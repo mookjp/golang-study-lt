aa10d01c02dab5b192ebabf64d81f280558f4d8c
mook <mookjpy@gmail.com>
2018-11-22 21:57:39 +0900

    tmp

--- ch11/ex03/main_test.go
+++ ch11/ex03/main_test.go
@@ -11,10 +11,29 @@ import (
 func randomPalindrome(rng *rand.Rand) string {
 	n := rng.Intn(25) // 24までのランダムな数字
 	runes := make([]rune, n)
+
+	// n = 9 のとき i は 5 以下
+	// n = 10 のとき i は 5 以下
+	// n = 11 のとき i は 6 以下
 	for i := 0; i < (n+1)/2; i++ {
 		r := rune(rng.Intn(0x1000)) // '\u0999' までのランダムなルーン
+		// runes の 頭から r を詰める
 		runes[i] = r
+		// n = 10 のとき 10-1-0=
+		// runes の お尻から r を詰める
 		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+// TODO: 正しくUnicode文字列を順番に作成する方法を調べる
+func randomStr(rng *rand.Rand) string {
+	n := rng.Intn(25) // 24までのランダムな数字
+	runes := make([]rune, n)
+	start := rng.Intn(0x1000)
+
+	for i := 0; i < n; i++ {
+		runes[i] = rune(start + i)
 	}
 	return string(runes)
 }
@@ -27,8 +46,24 @@ func TestRandomPalindromes(t *testing.T) {
 
 	for i := 0; i < 1000; i++ {
 		p := randomPalindrome(rng)
+		t.Logf("random palindrome: %s", p)
 		if !word.IsPalindrome(p) {
 			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
+
+func TestNonPalindromes(t *testing.T) {
+	// 疑似乱数生成器を初期化する
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomStr(rng)
+		//t.Logf("random string: %s", p)
+		if word.IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = true", p)
 		}
 	}
 }
