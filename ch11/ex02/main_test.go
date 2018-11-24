74ac3d31c37ba97b38fad92cdc71f290c8f42ab4
mook <mookjpy@gmail.com>
2018-11-25 03:52:45 +0900

    add ex2 test

--- ch11/ex02/main_test.go
+++ ch11/ex02/main_test.go
@@ -1,13 +1,55 @@
 package intset
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-func TestIntSetHas(t *testing.T) {
-
+func TestIntSetAdd(t *testing.T) {
 	var tests = []struct {
-		input int
+		input    map[int]int
+		expected string
 	}{
-		{1},
+		{map[int]int{1: 1}, "{1}"},
+		{map[int]int{1: 1, 2: 2, 3: 3}, "{1, 2, 3}"},
 	}
 
+	for _, test := range tests {
+		t.Logf("input: %v, expected: %v\n", test.input, test.expected)
+
+		inputKeySet := makeKeySetFromMap(test.input)
+		set := IntSet{}
+		for _, v := range inputKeySet {
+			set.Add(v)
+		}
+		inputKeySetStr := makeArrayString(inputKeySet)
+		setStr := set.String()
+
+		if inputKeySetStr != setStr {
+			t.Errorf("inputKeySetStr = %v, setStr = %v", inputKeySetStr, setStr)
+		}
+	}
+}
+
+func makeKeySetFromMap(m map[int]int) []int {
+	set := make([]int, 0, len(m))
+	for _, value := range m {
+		set = append(set, value)
+	}
+	return set
+}
+
+func makeArrayString(set []int) string {
+	res := "{"
+	for i, v := range set {
+		if i != 0 {
+			res = res + " " + strconv.Itoa(v)
+		} else {
+			res = res + strconv.Itoa(v)
+		}
+		if i == len(set)-1 {
+			res = res + "}"
+		}
+	}
+	return res
 }
